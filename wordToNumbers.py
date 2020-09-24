326e6e20ac479c5e7141b7ba0d346764cd094d90
superhu0 <38337398+superhu0@users.noreply.github.com>
2020-09-24 12:52:01 +0800

    Update wordToNumbers.py

--- wordToNumbers.py
+++ wordToNumbers.py
@@ -8,9 +8,15 @@ __author__ = 'Super Hu'
 #输入：cat（不区分大小写）
 #输出：228
 
-alphabeta = {'a':2, 'b':2, 'c':2, 'd':3, 'e':3, 'f':3, 'g':4, 'h':4, 'i':4, 'j':5, 'k':5, 'l':5, 'm':6, 'n':6, 'o':6, 'p':7, 'q':7, 'r':7, 's':7, 't':8, 'u':8, 'v':8, 'w':9, 'x':9, 'y':9, 'z':9, ' ':0}
-word = input('请输入英文单词： ').lower().strip()
-num = 0
-for a in word:
-    num = alphabeta[a] + num * 10
-print(num)
+def wordToNumber(word):
+    alphabeta = {'a':2, 'b':2, 'c':2, 'd':3, 'e':3, 'f':3, 'g':4, 'h':4, 'i':4, 'j':5, 'k':5, 'l':5, 'm':6, 'n':6, 'o':6, 'p':7, 'q':7, 'r':7, 's':7, 't':8, 'u':8, 'v':8, 'w':9, 'x':9, 'y':9, 'z':9, ' ':0}
+    word = word.lower().strip()
+    num = 0
+    for a in word:
+        num = alphabeta[a] + num * 10
+    return num
+
+if __name__ == '__main__':
+    word = input('Please input a word: ')
+    result = wordToNumber(word)
+    print(result)
